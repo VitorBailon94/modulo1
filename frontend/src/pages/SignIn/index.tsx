87ba57732d058213c3278799196908b76f87af04
Vitor Bailon <vitor.carulla@gmail.com>
2021-01-26 20:14:44 -0300

    Adicionando biblioteca unform

--- frontend/src/pages/SignIn/index.tsx
+++ frontend/src/pages/SignIn/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 
+import { Form } from '@unform/web';
+
 import Logo from '../../assets/logo.svg';
 
 import Input from '../../components/Input';
@@ -9,27 +11,32 @@ import Button from '../../components/Button';
 
 import { Container, Content, Background } from './styles';
 
-const SignIn: React.FC = () => (
-  <Container>
-    <Content>
-      <img src={Logo} alt="Logo IMG" />
-      <form>
-        <h1>Faça seu Logon</h1>
-        <Input name="email" icon={FiMail} placeholder="E-mail" />
+const SignIn: React.FC = () => {
+  function handleSubmit(data: object): void {
+    console.log(data)
+  }
+  return (
+    <Container>
+      <Content>
+        <img src={Logo} alt="Logo IMG" />
+        <Form onSubmit={handleSubmit}>
+          <h1>Faça seu Logon</h1>
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
 
-        <Input name="password" icon={FiLock} type="password" placeholder="Senha" />
+          <Input name="password" icon={FiLock} type="password" placeholder="Senha" />
 
-        <Button type="submit">Login</Button>
+          <Button type="submit">Login</Button>
 
-        <a href="forgot">Esqueci minha Senha</a>
-      </form>
-      <a href="login">
-        <FiLogIn />
-        Criar Conta
-      </a>
-    </Content>
-    <Background />
-  </Container>
-);
+          <a href="forgot">Esqueci minha Senha</a>
+        </Form>
+        <a href="login">
+          <FiLogIn />
+          Criar Conta
+        </a>
+      </Content>
+      <Background />
+    </Container>
+  )
+};
 
 export default SignIn;
