6131996d5134153b42c59cef05924de17beafba7
Vitor Bailon <vitor.carulla@gmail.com>
2021-02-03 17:09:43 -0300

    Fazendo validação da tela

--- frontend/src/pages/SignIn/index.tsx
+++ frontend/src/pages/SignIn/index.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-
+import React, { useCallback, useRef } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
-
+import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
-
+import * as Yup from 'yup';
+import getValidationsErrors from '../../utils/getValidationsErros';
 import Logo from '../../assets/logo.svg';
 
 import Input from '../../components/Input';
@@ -11,19 +11,46 @@ import Button from '../../components/Button';
 
 import { Container, Content, Background } from './styles';
 
+interface iData {
+  email: string;
+}
+
 const SignIn: React.FC = () => {
-  function handleSubmit(data: object): void {
-    console.log(data)
-  }
+  const formRef = useRef<FormHandles>(null);
+  const handleSubmit = useCallback(async (data: iData) => {
+    try {
+      formRef.current?.setErrors({});
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .required('E-mail obrigatório')
+          .email('Digite um e-mail valido'),
+        password: Yup.string().min(6, 'No minimo 5 digitos'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      const errors = getValidationsErrors(err);
+
+      formRef.current?.setErrors(errors);
+    }
+  }, []);
+
   return (
     <Container>
       <Content>
         <img src={Logo} alt="Logo IMG" />
-        <Form onSubmit={handleSubmit}>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Faça seu Logon</h1>
           <Input name="email" icon={FiMail} placeholder="E-mail" />
 
-          <Input name="password" icon={FiLock} type="password" placeholder="Senha" />
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Senha"
+          />
 
           <Button type="submit">Login</Button>
 
@@ -36,7 +63,7 @@ const SignIn: React.FC = () => {
       </Content>
       <Background />
     </Container>
-  )
+  );
 };
 
 export default SignIn;
